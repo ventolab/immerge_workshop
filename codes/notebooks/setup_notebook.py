da0f14c1e617a5352c6adf0900d1fb80561077cc
Antonio Parraga-Leo <antonioparragaleo@gmail.com>
2026-09-15 11:43:18 +0100

    setup indentation

--- codes/notebooks/setup_notebook.py
+++ codes/notebooks/setup_notebook.py
@@ -2,9 +2,9 @@ import os, sys, subprocess
 from pathlib import Path
 
 
-    course_dir = Path("/content/drive/MyDrive/immerge_workshop")
-    os.chdir(course_dir)
-    subprocess.run([sys.executable, "-m", "pip", "install", "-q",
-                    "-r", "codes/requirements.txt"], check=True)
+course_dir = Path("/content/drive/MyDrive/immerge_workshop")
+os.chdir(course_dir)
+subprocess.run([sys.executable, "-m", "pip", "install", "-q",
+                "-r", "codes/requirements.txt"], check=True)
 
 print(course_dir)
